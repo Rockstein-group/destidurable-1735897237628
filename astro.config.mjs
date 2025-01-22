cf05c344a0994eb178d3b62c646da34f66a49b5c
dtwnkhung <quangnhat249@gmail.com>
2025-01-22 09:37:46 +0700

    feat: update home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import compressor from "astro-compressor";
-
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compressor({ gzip: true, brotli: false })],
+  site: "https://destidurable.com",
+  integrations: [
+    tailwind(),
+    robotsTxt(),
+    sitemap(),
+    compressor({ gzip: true, brotli: false }),
+  ],
 });
